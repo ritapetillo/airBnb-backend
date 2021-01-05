39821b885afc7129f01ac16ea77a6ac4626e82f2
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-01-05 16:43:31 -0500

    updated booking schema

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const BookingSchema = new mongoose.Schema({
+  listing: { type: Schema.Types.ObjectId, ref: "listings" },
+  guest: { type: Schema.Types.ObjectId, ref: "users" },
+  reviewFromGuest: { type: Schema.Types.ObjectId, ref: "reviews" },
+  reviewFromHost: { type: Schema.Types.ObjectId, ref: "reviews" },
+  checkin: Date,
+  checkout: Date,
+  guests: Number,
+  totalAmount: Number,
+  createdAt: Date,
+  updatedAt: Date,
+});
 
-listing:{ type: Schema.Types.ObjectId, ref: 'listings' },
-guest:{ type: Schema.Types.ObjectId, ref: 'users' },
-reviewFromGuest:{ type: Schema.Types.ObjectId, ref: 'reviews' },
-reviewFromHost:{ type: Schema.Types.ObjectId, ref: 'reviews' },
-checkin:Date,
-checkout:Date,
-totalAmount:Number,
-createdAt:Date,
-updatedAt:Date
-})
-
-module.exports = mongoose.model('bookings',BookingSchema)
+module.exports = mongoose.model("bookings", BookingSchema);
